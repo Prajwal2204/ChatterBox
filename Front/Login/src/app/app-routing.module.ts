1735c522cb0b3d5a98f3d8ea305adf943d42b5e4
Prajwal Navada <44873030+Prajwal2204@users.noreply.github.com>
2020-11-04 14:58:23 +0530

    signup/register page added

--- Front/Login/src/app/app-routing.module.ts
+++ Front/Login/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SignupComponent } from './signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'signup', component: SignupComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [SignupComponent];
